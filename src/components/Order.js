43fd7e76f0ac49585dc27046f063b281c8c83e1e
Moti Elmakies <motiphone2003@gmail.com>
2020-12-01 16:29:00 +0200

    :apple: animate presence

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { motion } from 'framer-motion';
+import React, { useState } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
 
 const containerVariants = {
   hidden: {
@@ -30,6 +30,11 @@ const childVariants = {
 };
 
 const Order = ({ pizza }) => {
+  const [showTitle, setShowTitle] = useState(true);
+
+  setTimeout(() => {
+    setShowTitle(false);
+  }, 3000);
   return (
     <motion.div
       className='container order'
@@ -37,7 +42,13 @@ const Order = ({ pizza }) => {
       initial='hidden'
       animate='visible'
     >
-      <h2>Thank you for your order :)</h2>
+      <AnimatePresence>
+        {showTitle && (
+          <motion.h2 exit={{ y: '-100vh' }}>
+            Thank you for your order :)
+          </motion.h2>
+        )}
+      </AnimatePresence>
       <motion.p variants={childVariants}>
         You ordered a {pizza.base} pizza with:
       </motion.p>
